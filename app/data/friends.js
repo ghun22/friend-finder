5eff9e024b70d061b952b1989c12a1b53723ee78
Gun Lee <ghun22322@gmail.com>
2019-08-25 19:48:55 -0700

    final

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+        name: "ezreal",
+        photo: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ezreal_0.jpg",
+        scores: [1,2,3,4,5,1,4,2,4,2]
+    },
+    {
+        name: "nidalee",
+        photo: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nidalee_0.jpg",
+        scores: [1,2,5,5,3,4,2,1,4,5]
+    },
+    {
+        name: "lux",
+        photo: "https://cdn.vox-cdn.com/thumbor/hDD-fJ2EAhhbmbNEVCjwXZ-ZvgQ=/0x0:1280x755/920x613/filters:focal(538x276:742x480):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/52034985/elementalistlu.0.jpeg",
+        scores: [1,2,2,1,2,1,4,1,2,5]
+    },
+    {
+        name: "garen",
+        photo: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_13.jpg",
+        scores: [5,5,3,3,2,2,1,5,5,5]
+    },
+    {
+        name: "talon",
+        photo: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Talon_5.jpg",
+        scores: [4,2,3,2,3,4,2,1,2,4]
+    }
+];
+module.exports = friends;
